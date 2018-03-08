d3fb967d8cf00fcfed4b82a9086b679ad951e9ab
Sylvester Klirowski <klirowski.s@gmail.com>
2018-03-08 21:13:41 +0100

    Add files via upload

---
+++ js/assessment_page.js
@@ -0,0 +1,42 @@
+/* MEDICAL ASSESSMENT */
+var assessmentNextButton = document.querySelector('#assessment-next');
+var initialQuestion = document.querySelector('#initial-question');
+var subsequentQuestions = document.querySelector('#subsequent-questions');
+var subseqQuesText = document.querySelector('#subseq-ques-text')
+
+// hide intial question once aswered
+// and show template for subsequent questions 
+assessmentNextButton.addEventListener('click', function (e) {
+   e.preventDefault();
+   console.log(e);
+   initialQuestion.setAttribute('hidden', 'true');
+   subsequentQuestions.removeAttribute('hidden');
+   nextQuestion();
+});
+
+// Yes, No, Skip buttons
+var yesBtn = document.querySelector('#yes-btn');;
+var noBtn = document.querySelector('#no-btn');;
+var skipBtn = document.querySelector('#skip-btn');;
+yesBtn.addEventListener('click', nextQuestion);
+noBtn.addEventListener('click', nextQuestion);
+skipBtn.addEventListener('click', nextQuestion);
+
+// random questions
+var randomQuestions = ["Do you have a fever?","Do you have chills?","Have you had this problem more than 72 hours?","Do you have diarrhea?","Have you lost weight?","Do you have general weakness?","Do you feel tired?","Do you have swollen glands?"];
+var count = 0;
+
+// keep track of count and set correct question
+function nextQuestion(){
+   if ((count > 0) && (count < randomQuestions.length)){
+      subseqQuesText.textContent = randomQuestions[count];
+      count += 1;
+   }
+   // reset count 
+   else {
+      count = 0;
+      subseqQuesText.textContent = randomQuestions[count];
+      count += 1;
+   }
+}
+
---
+++ js/index_page.js
@@ -0,0 +1,41 @@
+/* Toggle header text: mobile VS tablet, desktop */
+
+var introLeadIn = document.querySelector('.intro-lead-in');
+var introHeading = document.querySelector('.intro-heading');
+
+function toggleHeadingText() {
+   var emptyPara = document.createElement('p');
+   emptyPara.textContent = "";
+   if (window.innerWidth < 768) {
+      introLeadIn.textContent = 'Concerned about your health?';
+      introHeading.textContent = '';
+   } else {
+      introLeadIn.textContent = 'Do you have a Health Concern?';
+      introHeading.textContent = 'We can help';
+   }
+}
+window.onload = toggleHeadingText;
+window.onresize = toggleHeadingText;
+
+
+/* DIALOG MODAL */
+(function () {
+   var disclaimerLink = document.querySelector('#disclaimer-link');
+   var closeDisclaimer = document.querySelector('button.close');
+
+   // take out unnecessary dialog box close button
+   disclaimerLink.addEventListener('click', function () {
+      closeDisclaimer.parentElement.removeChild(closeDisclaimer);
+   });
+})();
+
+/* HEALTH ASSESSMENT CARD */
+window.onload = function () {
+   // add years to DOB select box
+   var dateOfBirth = document.querySelector("#dob");
+   for (var i = 2006; i > 1918; i--) {
+      var year = document.createElement('option');
+      year.textContent = i;
+      dateOfBirth.appendChild(year);
+   }
+}
